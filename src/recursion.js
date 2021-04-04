cdb6d3df10780a2c683cca17ba72f8e3540cd943
Aschale Siyoum <AschaleS@users.noreply.github.com>
2021-04-04 00:18:59 -0500

    Update recursion.js
    Comments

--- src/recursion.js
+++ src/recursion.js
@@ -18,6 +18,7 @@ var factorial = function(n) {
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
+// Sum(123, 345)
 var sum = function(array) {
   if(array.length === 0){
     return 0;
